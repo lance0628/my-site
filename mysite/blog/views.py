d7b72a68bf82558c04262d258ed44243345b9acb
lance0628 <53386065+lance0628@users.noreply.github.com>
2019-08-06 01:40:07 +0800

    login management

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,9 +7,9 @@ from .models import BlogArticles
 
 def blog_title(request):
     blogs = BlogArticles.objects.all()
-    return render(request, "blog/title.html",{"blogs":blogs})
+    return render(request, "blog/titles.html",{"blogs":blogs})
 def blog_article(request, article_id):
-    #article = BlogArticles.objects.get(id=article_id)
+    article = BlogArticles.objects.get(id=article_id)
     aritcle = get_object_or_404(BlogArticles, id=article_id)
     pub = article.publish
     return render(request, "blog/content.html",{"article":article, "publish":pub })
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates'),],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+LOGIN_REDIRECT_URL = '/blog/'
