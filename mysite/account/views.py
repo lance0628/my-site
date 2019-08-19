d4b0407446c39928fa435613f8ee50f014397137
lance0628 <53386065+lance0628@users.noreply.github.com>
2019-08-19 21:25:35 +0800

    add register

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -33,7 +33,7 @@ def user_login(request):
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
             cd =login_form.cleaned_data
-            user = authenticate(username=cd['username'],password=cd["password"])
+            user = authenticate(username=cd['username'],password=cd['password'])
 
             if user:
                 login(request,user)
@@ -46,3 +46,19 @@ def user_login(request):
     if request.method == "GET":
         login_form = LoginForm()
         return render(request,"account/login.html",{"form":login_form})
+
+from .forms import LoginForm, RegistrationForm
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponse("successfully")
+        else:
+            return HttpResponse("sorry, you can not register.")
+    else:
+        user_form = RegistrationForm()
+        return render(request, "account/register.html",{"form": user_form})
